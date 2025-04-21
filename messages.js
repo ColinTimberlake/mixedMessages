26e0dbf9d72edf80938459d43c5b8101c1397527
Colin <colint1993@yahoo.com>
2025-04-20 21:58:57 -0500

    added JavaScript beginning stages

---
+++ messages.js
@@ -0,0 +1,13 @@
+/*
+Ideas: Joke Generator
+store the joke and the punchline in seperate containers
+select one random joke and one random punchline
+main functionality requirements:
+the JavaScript functioning correctly
+able to produce a message each time
+extra:
+work on a UI for users to interact with
+works with a button push */
+
+const jokes={};
+const punchlines={}
