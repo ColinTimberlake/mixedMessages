ad6fbb5aea18018baf583cc04c1a02330736e517
Colin <colint1993@yahoo.com>
2025-04-21 15:34:48 -0500

    added code to make the JavaScript portion work

--- messages.js
+++ messages.js
@@ -9,5 +9,10 @@ extra:
 work on a UI for users to interact with
 works with a button push */
 
-const jokes={};
-const punchlines={}
+const jokes=['Why did the chicken cross the road?', 'What is black and white and red all over?', 
+    'What did the fish say when he ran into a wall?', 'What do you call a fish with no eyes?'];
+const punchLines=['to get to the other side', 'A newspaper', 'Dam', 'a fsh'];
+
+let a=jokes[Math.floor(Math.random()*jokes.length)];
+let b=punchLines[Math.floor(Math.random()*jokes.length)];
+console.log(a+' '+b);
